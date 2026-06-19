372c0f470a3c95377d8660101f056f384b746e3c
Naumov <naumov.a.n@mail.ru>
2026-06-19 14:18:36 +0700

    cmake file

--- build/CMakeFiles/test-shared-matrix.dir/src/test/test-shared-matrix.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/test-shared-matrix.dir/src/test/test-shared-matrix.cpp.o: \
- /home/naumov/Documents/skillbox/cpp/hw6/src/test/test-shared-matrix.cpp \
- /usr/include/stdc-predef.h \
- /home/naumov/Documents/skillbox/cpp/hw6/include/export.h \
- /home/naumov/Documents/skillbox/cpp/hw6/build/export_shared.h
